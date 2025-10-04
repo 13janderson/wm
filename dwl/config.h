2f0d4daa9bdb2bb4b04bb51e412ea4fadc956044
Jack Anderson <jackanderson02@outlook.com>
2025-10-04 23:11:38 +0100

    screenshots work

--- dwl/config.h
+++ dwl/config.h
@@ -188,7 +188,7 @@ static const enum libinput_config_tap_button_map button_map =
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
   {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+    .v = (const char *[]) { "/usr/bin/zsh", "-c", cmd, NULL }                       \
   }
 
 /* commands */
