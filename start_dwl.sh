2e8a4e73f8c441155a25de3471c5ff12ff705c0e
Jack Anderson <jackanderson02@outlook.com>
2025-10-04 16:08:30 +0100

    move dwl start to here

---
+++ start_dwl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# /usr/bin/pipewire &
+# /usr/bin/pipewire-pulse &
+# /usr/bin/wireplumber &
+# /usr/bin/pipewire-media-session &
+
+slstatus -s | dwl -s "sh -c 'swaybg -i $HOME/wallpapers/wallhaven-tree.jpg'"
